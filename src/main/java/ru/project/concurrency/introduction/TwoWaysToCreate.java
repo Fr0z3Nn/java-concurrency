50e136b543803b85f4ce9849f0f23376a0a9a106
Fr0z3Nn <luckyjohn057@gmail.com>
2021-03-08 15:58:13 +0300

    join

--- src/main/java/ru/project/concurrency/introduction/TwoWaysToCreate.java
+++ src/main/java/ru/project/concurrency/introduction/TwoWaysToCreate.java
@@ -1,15 +1,29 @@
 package ru.project.concurrency.introduction;
 
+import sun.management.snmp.jvminstr.JvmThreadInstanceEntryImpl;
+
 public class TwoWaysToCreate {
-    public static void main(String[] args) {
-        new Thread(new Runner1()).start();
-        new Runner2().start();
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new Runner1());
+        t1.start();
+        Thread t2 = new Runner2();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Finish all threads....");
     }
 }
 
 class Runner1 implements Runnable{
     public void run() {
-        for(int i=0;i<10;++i){
+        for(int i=0;i<5;++i){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("Runner1: " + i);
         }
     }
@@ -17,7 +31,12 @@ class Runner1 implements Runnable{
 
 class Runner2 extends Thread{
     public void run() {
-        for(int i=0;i<10;++i){
+        for(int i=0;i<5;++i){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("Runner2: " + i);
         }
     }
